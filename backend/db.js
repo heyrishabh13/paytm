1a259f09e86315c254ad51aac626adb312fc3e86
Rishabh Kamboj <heyrishabh13@gmail.com>
2025-07-15 15:42:45 +0530

    User schema added

---
+++ backend/db.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    require: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    require: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    require: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+  User,
+};
